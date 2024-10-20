84ddab419097fd091fb8eafa09fd16c79f385708
DeshonS <deshonjewell@gmail.com>
2024-10-20 10:42:16 -0500

    fixed spelling of PRIVILEGES

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- set all priviledges on database for user
-GRANT ALL PRIVILEDGES ON hbnb_dev_db to 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db to 'hbnb_dev'@'localhost';
 
 -- set select priviledges on a set database for user
 GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
