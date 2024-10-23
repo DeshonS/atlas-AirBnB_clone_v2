3f37c5bbbdc40ef456f0735b25c8f2bba96e2951
DeshonS <deshonjewell@gmail.com>
2024-10-23 16:27:03 +0000

    updated users

--- models/user.py
+++ models/user.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
-
+from alembic import op
+import sqlalchemy as sqla
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = op.create_table('users')
+    email = sqla.Column('email', sqla.string(), nullable=False)
+    password = sqla.Column('password', sqla.string(), nullable=False)
+    first_name = sqla.Column('first_name', sqla.string(), nullable=False)
+    last_name = sqla.Column('last_name', sqla.string(), nullable=False)
