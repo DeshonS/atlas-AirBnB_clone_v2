1fc8655a10d5555a39f1b57adbf106882b4d162f
Deshon Stout <46934055+DeshonS@users.noreply.github.com>
2024-11-02 11:19:44 -0500

    added 2 blank lines between app route and define

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-
-
 """Flask module that returns Hello HBNB"""
 
 
 from flask import Flask
 app = Flask(__name__)
 @app.route('/', strict_slashes=False)
+
+
 def hello_hbnb():
     return "Hello HBNB!"
 
