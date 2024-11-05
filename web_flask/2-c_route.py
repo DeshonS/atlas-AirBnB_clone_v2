376f61ea3370fdfbd8619ccc2e81753770bc0efe
Deshon Stout <46934055+DeshonS@users.noreply.github.com>
2024-11-05 14:05:01 -0600

    testing slash reduction

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -22,7 +22,7 @@ def c_text(text):
     return f"C {text}"
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
     text = text.replace('_', ' ')
