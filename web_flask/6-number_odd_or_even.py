6646929822645a4e1bb8ad69e15ef55ba2be2774
Deshon Stout <46934055+DeshonS@users.noreply.github.com>
2024-11-06 10:11:31 -0600

    shortened line 45

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -42,7 +42,8 @@ def html_int(n):
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even(n):
     oddeven = "even" if n % 2 == 0 else "odd"
-    return render_template('6-number_odd_or_even.html', number=n, oddeven=oddeven)
+    return render_template('6-number_odd_or_even.html', number=n,
+                           oddeven=oddeven)
 
 
 if __name__ == "__main__":
