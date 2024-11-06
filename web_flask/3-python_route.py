542b5f8796718a4b896f4cd5b321a09b102d34a8
Deshon Stout <46934055+DeshonS@users.noreply.github.com>
2024-11-06 08:49:34 -0600

    push

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,8 +22,7 @@ def c_text(text):
     return f"C {text}"
 
 
-@app.route('/python/0', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/<text>', defaults={'text': 'is cool'}, strict_slashes=False)
 def python_text(text):
     text = text.replace('_', ' ')
     return f"Python {text}"
